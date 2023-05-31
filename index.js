6a34219bf959c0f9456fcc11090bca52666d32b2
Raghu Rayamajhi <ryamacoding@outlook.com>
2023-05-31 17:57:04 +0100

    adding burger menu

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function showErrorMessage() {
     alert('This feature has yet to be implemented');
   };
+
+  function openMenu(){
+    document.body.classList += " menu--open";
+  }
+
+  function closeMenu(){
+    document.body.classList.remove('menu--open');
+  }
