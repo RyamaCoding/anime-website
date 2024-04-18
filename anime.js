eba4af2e818828e7ed42599d8a79781e7f9475a3
Raghu Rayamajhi <ryamacoding@outlook.com>
2024-04-18 17:29:14 +0100

    brought back loading

--- anime.js
+++ anime.js
@@ -53,7 +53,7 @@ function closeMenu() {
 
   function startLoading() {
     const button = document.getElementById("search-button");
-    button.classList += ("loading");
+    button.classList += (" loading");
   }
 
   function stopLoading() {
